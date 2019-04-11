32533c4e87678a46e187843914aaea32ecdb6f55
Ethan Davis <futuristiccake@gmail.com>
2019-04-11 15:43:00 -0700

    Removed fe bundle from package

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	'entry': './index.js',
 	'output': {
 		'path': path.join(__dirname, 'dist'),
-		'filename': 'bundle.js',
+		'filename': 'protocore.js',
 		'library': 'protocore'
 	}
 }
